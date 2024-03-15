3842c21f520c66287c381de8f3fb68608bba0708
vietviet08 <145907593+vietviet08@users.noreply.github.com>
2024-03-15 22:59:28 +0700

    add project

---
+++ untitled/Admin/src/main/java/com/admin/config/AdminServiceConfig.java
@@ -0,0 +1,29 @@
+package com.admin.config;
+
+import com.library.model.Admin;
+import com.library.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+@Service
+public class AdminServiceConfig implements UserDetailsService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Admin admin = adminRepository.findByUsername(username);
+        if (admin == null) throw new UsernameNotFoundException("Not found Admin by username " + username);
+        return new User(admin.getUsername(), admin.getPassword(), admin.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList()));
+    }
+}
---
+++ untitled/Library/src/main/java/com/library/model/Customer.java
@@ -0,0 +1,6 @@
+package com.library.model;
+
+public class Customer {
+
+
+}
